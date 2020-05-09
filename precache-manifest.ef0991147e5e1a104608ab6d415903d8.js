3d678f98a95138183a750bcfb3b718daa13918c2
salkahtani1.c <salkahtani1.c@ksu.edu.sa>
2020-05-09 22:20:17 +0300

    Updates

---
+++ precache-manifest.ef0991147e5e1a104608ab6d415903d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b3408b70ccc5b99e822cd48583df8e",
+    "url": "/react-post-get/index.html"
+  },
+  {
+    "revision": "586d858784d51f6ac688",
+    "url": "/react-post-get/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "60cf58be051394ca63dd",
+    "url": "/react-post-get/static/js/2.54e25841.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-post-get/static/js/2.54e25841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "586d858784d51f6ac688",
+    "url": "/react-post-get/static/js/main.6e52f961.chunk.js"
+  },
+  {
+    "revision": "b127af7dc5f624952fb6",
+    "url": "/react-post-get/static/js/runtime-main.7b9ba478.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-post-get/static/media/logo.5d5d9eef.svg"
+  }
+]);
